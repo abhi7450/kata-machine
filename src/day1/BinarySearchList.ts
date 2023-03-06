3c35e982040dfada808550f280255a96810b86a0
Abhishek A <abhishekashok64@gmail.com>
2023-03-06 16:25:43 +0530

    Binary Sort and Insertion Sort

--- src/day1/BinarySearchList.ts
+++ src/day1/BinarySearchList.ts
@@ -1,3 +1,18 @@
+function bs(arr: number[], needle: number, lo: number, hi: number): boolean {
+    let mid = lo + (hi - lo) / 2;
+    if (lo > hi) {
+        return false;
+    }
+    if (arr[mid] === needle) {
+        return true;
+    }
+    if (arr[mid] < needle) {
+        bs(arr, needle, mid + 1, hi);
+    } else {
+        bs(arr, needle, lo, mid - 1);
+    }
+    return false;
+}
 export default function bs_list(haystack: number[], needle: number): boolean {
-
+    return bs(haystack, needle, 0, haystack.length - 1);
 }
--- src/day1/InsertionSort.ts
+++ src/day1/InsertionSort.ts
@@ -1,12 +1,10 @@
 export default function InsertionSort(arr: number[]) {
-    let n = arr.length - 1;
+    let n = arr.length;
     for (let i = 1; i < n; i++) {
         let key = arr[i];
         let j;
-        for (j = i - 1; j >= 0; j--) {
-            if (key < arr[j]) {
-                arr[j + 1] = arr[j];
-            }
+        for (j = i - 1; j >= 0 && key < arr[j]; --j) {
+            arr[j + 1] = arr[j];
         }
         arr[j + 1] = key;
     }
